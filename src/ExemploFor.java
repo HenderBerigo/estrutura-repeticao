02d0f0b875e99ad95c57f14f81c609f9d2d4c16a
Hender Berigo <henderson.berigo@gmail.com>
2025-02-13 08:42:35 -0300

    for break continue

--- src/ExemploFor.java
+++ src/ExemploFor.java
@@ -3,10 +3,15 @@ public class ExemploFor {
         for(int carneirinhos = 1; carneirinhos <=20; carneirinhos++){
             if(carneirinhos == 10){
                 System.out.println("Quase dormindo");
+                continue;
+            }
+
+            if (carneirinhos == 20) {
+                System.out.println("Dormiu");
+                continue;
             }
             System.out.println("Contando carneirinhos: "+carneirinhos);
         }
 
-        System.out.println("Dormiu");
     }
 }
