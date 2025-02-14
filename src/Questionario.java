0e8dac0a01f3782896467fe4f4a5fef07333ab39
Hender Berigo <henderson.berigo@gmail.com>
2025-02-14 07:39:45 -0300

    questoes estrutura de repeticao

---
+++ src/Questionario.java
@@ -0,0 +1,23 @@
+public class Questionario {
+    public static void main(String[] args) {
+        // int numero = 1;
+        // for (int x=1; x<2; x++){
+        //     numero = numero + x;
+        // }
+        // System.out.println("O valor de número é: " + numero);
+
+        // for(int x=1;x<=10;x++){
+        //     if(x % 2==0) //% operador que representa módulo de uma divisão
+        //     System.out.println(x); }
+
+        // boolean condicao = false;
+        // while(condicao){
+        //     System.out.println("executou ... ");
+        // }
+
+        int num = 5, count = 1;
+        do {
+            num += count; System.out.println(num);
+        } while (count <= 3);
+    }
+}
